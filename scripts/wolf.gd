ea8d72034c1ed9ae965b94af92739adfa2f65782
Yechann Kim <dpcks9677@naver.com>
2023-11-20 11:38:00 +0900

    Walker, Wolf 공격 애니메이션 추가
    Area2D에서 충돌 판정 시, 멈춘 후 공격애니메이션을 재생.
    
    ++ 타이머로 공격시간 조절 필요

--- scripts/wolf.gd
+++ scripts/wolf.gd
@@ -10,9 +10,10 @@ func _physics_process(delta):
 		engage()
 
 func move(delta):
-	#우측 자동이동 및 걷기 애니메이션 
-	velocity.x = speed
+	#좌측 자동이동 및 걷기 애니메이션 
+	velocity.x = -speed
 	velocity.y = 0
+	$AnimatedSprite2D.flip_h = false
 	$AnimatedSprite2D.play("walk")
 	
 	move_and_slide()
@@ -25,13 +26,15 @@ func engage():
 	$AnimatedSprite2D.play("attack")
 	
 	move_and_slide()
+	
 
-func ally():
+func enemy():
 	pass
 
-func _on_area_2d_body_entered(body):
-	if body.has_method("enemy"):
+func _on_attack_range_body_entered(body):
+	if body.has_method("ally"):
 		state = "engage"
-	
-func _on_area_2d_body_exited(body):
+
+
+func _on_attack_range_body_exited(body):
 	state = "move"
