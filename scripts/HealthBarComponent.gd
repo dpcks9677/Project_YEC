4cec0b04a10d70e69bd943951b977c79b4dadd53
Yechann Kim <dpcks9677@naver.com>
2024-01-15 09:15:37 +0900

    branch test
    branch test

--- scripts/HealthBarComponent.gd
+++ scripts/HealthBarComponent.gd
@@ -5,13 +5,13 @@ class_name healthBarComponent
 @export var health : int
 
 func _ready():
-	#engageComponent에서 health 받아오기
+	#engageComponent에서 health 받아오기 + 체력바 최대치 설정
 	health = engage_Component.health
 	$healthBar.max_value = health
 
 func _process(_delta):
 	setHealth()
 
-func setHealth():
+func setHealth(): #체력바 갱신 
 	health = engage_Component.health
 	$healthBar.value = health
