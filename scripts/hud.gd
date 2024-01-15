64f99ef9aa60f7036bdb30b0490538d1c94edf82
Yechann Kim <dpcks9677@naver.com>
2024-01-15 15:29:11 +0900

    resourceHandler 추가 및 파일경로 재설정
    * resourceHandler에서 마나를 관리하고 이를 HUD에서 참조해 출력 할 수 있도록 간단하게 UI를 구현함
    
    * unit/component 폴더를 system/component 로 이동함

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-
+#유닛 씬 형식 선언 
 @export var slot_1 : PackedScene
 @export var slot_2 : PackedScene
 @export var slot_3 : PackedScene
@@ -10,13 +10,20 @@ extends CanvasLayer
 @export var slot_7 : PackedScene
 @export var slot_8 : PackedScene
 
+#resourceHandler 형식 선언
+@export var rsc : resourceHandler
+
 func _ready():
+	#slot 불러 올 때, 직접 경로를 적지 않고, 데이터를 인계받아서 자리에 맞게 유닛을 매치 할 수 있도록 설계 
 	slot_1 = preload("res://units/ally/spearman.tscn")
 	slot_2 = preload("res://units/ally/knight.tscn")
+	
+	rsc = get_tree().get_root().get_node("stage1").get_node("resourceHandler")
 
 func _process(delta):
-	pass
+	displayMana()
 
+#unitSommoner 관련 스크립트  
 func spawn(unit):
 	#인스턴스화
 	var target = unit.instantiate()
@@ -34,3 +41,7 @@ func _on_slot_1_button_up():
 
 func _on_slot_2_button_up():
 	spawn(slot_2)
+
+#manaUI 관련 스크립트 
+func displayMana():
+	get_node("manaUI").get_node("manaLabel").text = str("your mana: ", rsc.current_mana)
---
+++ scripts/resourceHandler.gd
@@ -0,0 +1,64 @@
+extends Node
+class_name resourceHandler
+
+#마나 
+@export var mana_lv : int
+@export var max_mana : int 
+@export var regen_mana : int 
+@export var current_mana : int
+@export var isManaCooldown : bool
+
+#공격력 계수 
+@export var atk_lv : int
+@export var atk_multiplier : float
+
+#인구 수 
+@export var population_lv : int
+@export var population : int 
+
+func _ready():
+	#고정 값 대신 stage 별 기본 정보들을 참조해서 불러올 수 있도록 설계하기 
+	
+	#마나
+	mana_lv = 1
+	max_mana = 25
+	regen_mana = 2
+	current_mana = 0
+	
+	#공격력 
+	atk_lv = 1
+	atk_multiplier = 1.0
+	
+	#인구 수 
+	population_lv = 1
+	population = 10
+
+func _process(delta):
+	atkHandler()
+	populationHandler()
+	manaHandler()
+	
+#마나에 관한 기능들을 처리 
+func manaHandler():
+	if isManaCooldown == false:
+		if current_mana + regen_mana >= max_mana: #현재마나 + 재생 될 마나가 최대 마나보다 크다면 
+			current_mana = max_mana
+		else:
+			current_mana += regen_mana
+		$manaGenerator.start(1) #마나는 1초당 생성되므로 고정 값을 사용 함 
+		isManaCooldown = true
+	else: #마나가 쿨다운 중일 때는 아무것도 안 함.
+		pass
+
+func _on_mana_generator_timeout():
+	isManaCooldown = false
+	
+#공격력에 관한 기능들을 처리 
+func atkHandler():
+	pass
+
+#인구수에 관한 기능들을 처리 
+func populationHandler():
+	pass
+
+
