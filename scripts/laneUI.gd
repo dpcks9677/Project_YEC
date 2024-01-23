dc0aea18d486e77d4062c90ceafe2f66e58a89f2
Yechann Kim <dpcks9677@naver.com>
2024-01-24 01:00:28 +0900

    laneSelector 포맷 구현
    * 이전 버전에 있던 문제 해결함. 코드 집어넣고 수정하면 됨.

--- scripts/laneUI.gd
+++ scripts/laneUI.gd
@@ -1,12 +1,12 @@
 extends Control
 
 func _ready():
-	get_node("laneSelector").button_pressed = true
-	
+	get_node("laneSelector").text = str("btm")
+
 func _on_lane_selector_toggled(toggled_on):
 	if toggled_on == true:
 		print("top")
-		get_node("laneSelector").text = str("top")
+		$laneSelector.text = "top"
 	else:
 		print("btm")
-		get_node("laneSelector").text = str("btm")
+		$laneSelector.text = "btm"
