19684e232aa8d4e816f9871f297c91f0864d3b58
Yechann Kim <dpcks9677@naver.com>
2024-02-09 01:24:42 +0900

    baseHealthBar 구현
    * 아군, 적군 베이스의 체력을 디스플레이 하도록 만듦.

---
+++ scripts/baseHealthBar.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@export var rsc : resourceHandler
+
+@export var ALLY_MAX_HEALTH : int
+@export var ENEMY_MAX_HEALTH : int
+@export var allyHealth : int
+@export var enemyHealth : int
+
+
+func _ready():
+	rsc = get_tree().get_root().get_node("stage1").get_node("resourceHandler")
+	
+	ALLY_MAX_HEALTH = rsc.allyBaseHealth
+	ENEMY_MAX_HEALTH = rsc.enemyBaseHealth
+		
+	$allyHealthBar.max_value = ALLY_MAX_HEALTH
+	$enemyHealthBar.max_value = ENEMY_MAX_HEALTH
+	$allyHealthBar.value = rsc.allyBaseHealth
+	$enemyHealthBar.value = rsc.enemyBaseHealth
+
+func _physics_process(delta):
+	healthSetter()
+
+func healthSetter():
+	$allyHealthBar.value = rsc.allyBaseHealth
+	$allyHealthLabel.text = str(rsc.allyBaseHealth)
+	$enemyHealthBar.value = rsc.enemyBaseHealth
+	$enemyHealthLabel.text = str(rsc.enemyBaseHealth)
