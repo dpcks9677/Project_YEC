db4b480dc667b131d423747491f1a8903de449f5
Yechann Kim <dpcks9677@naver.com>
2024-04-15 15:01:43 +0900

    savefile 폼 작업
    * 세이브 파일 기초 작업 완료
    * 세이브파일은 사용자 데이터 폴더에 저장됨.

--- Content/Scenes/mainMenu.gd
+++ Content/Scenes/mainMenu.gd
@@ -1,5 +1,60 @@
 extends Control
 
+const savePath = "user://Saves/"
+const saveFile = "save1.json"
+const SECURITY_KEY = "intheafterglow"
+
+var savedata = saveData.new()
+
+func _ready():
+	verify_save_directory(savePath)
+
+func verify_save_directory(path : String):
+	DirAccess.make_dir_absolute(path)
+	
+func save_data(path : String):
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	if file == null:
+		print(FileAccess.get_open_error())
+		return
+		
+	var data = {
+		"save_data": {
+			"gold" : savedata.gold,
+			"currentStage" : savedata.currentStage,
+			"playTime" : savedata.playTime
+		}
+	}
+
+	var json_string = JSON.stringify(data, "\t")
+	file.store_string(json_string)
+	file.close()
+
+func load_data(path : String):
+	if FileAccess.file_exists(path):
+		var file = FileAccess.open(path, FileAccess.READ)
+		if file == null:
+			print(FileAccess.get_open_error())
+			return
+		
+		var content = file.get_as_text()
+		file.close()
+		
+		var data = JSON.parse_string(content)
+		if data == null:
+			printerr("Cannot parse %s as a json string: (%s)!" %[path, content])
+			return
+		
+		savedata = saveData.new()
+		savedata.gold = data.save_data.gold
+		savedata.currentStage = data.save_data.currentStage
+		savedata.playTime = data.save_data.playTime
+		
+		
+	else:
+		printerr("Cannot open non-existent file at %s!" %[path])
+
+#Signals
 func _on_quit_button_pressed():
 	get_tree().quit()
 
@@ -8,3 +63,10 @@ func _on_continue_button_pressed():
 
 func _on_back_button_pressed():
 	get_node("LoadScene").visible = false
+
+func _on_button_1_pressed():
+	load_data(savePath+saveFile)
+	get_node("LoadScene").get_node("saveInfo").text = str(savedata.gold)
+
+func _on_new_game_button_pressed():
+	save_data(savePath+saveFile)
---
+++ Content/Scripts/saveData.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name saveData
+
+@export var gold : int
+@export var currentStage : int
+@export var playTime : int
+
