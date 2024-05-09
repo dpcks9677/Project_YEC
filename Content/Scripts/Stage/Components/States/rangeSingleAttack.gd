c5b029cae6371e4087bcb4c72d0dd2c2de3c06e4
Yechann Kim <dpcks9677@naver.com>
2024-05-10 03:35:44 +0900

    projectile.gd 스크립트 수정
    - 연산 수정

--- Content/Scripts/Stage/Components/States/rangeSingleAttack.gd
+++ Content/Scripts/Stage/Components/States/rangeSingleAttack.gd
@@ -16,15 +16,15 @@ func _enter_tree():
 
 func _ready():
 	if get_parent().get_parent().get_parent().get_parent().get_name() == "topLane":
-		get_node("arrow").set_collision_mask_value(1, false)
-		get_node("arrow").set_collision_mask_value(2, true)
-		get_node("arrow").set_collision_layer_value(1, false)
-		get_node("arrow").set_collision_layer_value(2, true)
+		get_node("projectile").set_collision_mask_value(1, false)
+		get_node("projectile").set_collision_mask_value(2, true)
+		get_node("projectile").set_collision_layer_value(1, false)
+		get_node("projectile").set_collision_layer_value(2, true)
 	elif get_parent().get_parent().get_parent().get_parent().get_name() == "bottomLane":
-		get_node("arrow").set_collision_mask_value(1, false)
-		get_node("arrow").set_collision_mask_value(3, true)
-		get_node("arrow").set_collision_layer_value(1, false)
-		get_node("arrow").set_collision_layer_value(3, true)
+		get_node("projectile").set_collision_mask_value(1, false)
+		get_node("projectile").set_collision_mask_value(3, true)
+		get_node("projectile").set_collision_layer_value(1, false)
+		get_node("projectile").set_collision_layer_value(3, true)
 
 func Enter():
 	pass
@@ -46,12 +46,3 @@ func waiting_animation(): #await 키워드와 함께 사용
 
 func get_damage():
 	return get_parent().get_parent().attack_damage
-
-func _on_arrow_area_entered(area):
-	if area == castTarget and is_instance_valid(area):
-		castTarget.get_parent().get_node("stateComponent").damaged(damage)
-		if area.get_parent().get_node("stateComponent").property_exists(area.get_parent().get_node("stateComponent"), "health"):
-			print(area.get_parent().get_node("stateComponent").health)
-	else:
-		#print(castTarget)
-		pass
--- Content/Scripts/Stage/Components/projectile.gd
+++ Content/Scripts/Stage/Components/projectile.gd
@@ -2,81 +2,53 @@ extends Area2D
 
 # Points and movement parameters
 var start_point = position
-var end_point = Vector2(0,0)
-var height = 150.0
+var end_point = Vector2.ZERO
+var height = 200.0
 var duration = 0.5  # Duration of the movement in seconds
-var elapsed_time = 0.0 # = x
+var elapsed_time = 0.0
 
-var FIXED_VALUE = 200.0
-
-var directionValue : int #투사체 방향 
+var directionValue : int
+var FIXED_VALUE = 100.0
 
 var isFire : bool
 var isHitted : bool
 
 func _ready():
-	position = start_point  # Start at the first point
-	
+	#position = start_point  # Start at the first point
 	isFire = false
 	isHitted = false
 	visible = true
-	
-	#투사체 방향 결정 (ally면 left to right, enemy면 right to left)
-	if get_parent().get_parent().get_parent().get_faction() == "ally":
-		directionValue = 1
-	else:
-		directionValue = -1
 
 func _process(delta):
-	if get_parent().castTarget != null:
-		get_start_point()
-		end_point = get_parent().castTarget.get_parent().global_position
-		end_point.x += FIXED_VALUE
-	
 	if isFire == true:
 		if isHitted == false:
 			visible = true
 		else:
-			visible = false
-			get_node("CollisionShape2D").disabled = true
-			
+			visible = true
+	
+		end_point = get_parent().castTarget.get_node("CollisionShape2D").global_position - $"../../../../hitbox/CollisionShape2D".global_position
+		print(get_parent().castTarget.get_node("CollisionShape2D").global_position, $"../../../../hitbox/CollisionShape2D".global_position)
+		
+		if get_parent().get_parent().get_parent().get_faction() == "ally":
+			end_point.x += FIXED_VALUE
+		
 		if elapsed_time < duration:
 			elapsed_time += delta
-			
-			var length = abs(start_point.x - end_point.x)
-			var t = elapsed_time / duration  # Normalized time from 0 to 1
-			var x = elapsed_time * length / duration
-			var y = -height * sin(t * PI)
-			var angle = -cos(t * PI)
+			position = Vector2((end_point.x * (elapsed_time/duration)), (end_point.y * (elapsed_time/duration)) - sin(elapsed_time/duration * PI) * height)
+		
+			var angle = -cos(elapsed_time * 2 * PI)
 			set_rotation(angle)
-
-			position = Vector2(directionValue * x, y)  # Update position
 		else:
-			#투사체 노드 위치 초기화 및 변수 초기화 
 			position = Vector2(0,0) # Ensure the sprite ends at the exact end point
-			height = randf_range(120, 160)
-			
-			elapsed_time = 0.0 
-			position = start_point
-			visible = false
-			isHitted = false
+			visible = true #원래 false 
 			isFire = false
-			
-			get_node("CollisionShape2D").disabled = true
-			
 			set_rotation(0)
-			
+			elapsed_time = 0.0
+			position = start_point
+	
 func switchFire():
 	isFire = true
 
-func get_start_point():
-	start_point = get_parent().get_parent().get_parent().get_parent().global_position
-
-func get_end_point():
-	if get_parent().castTarget != null:
-		end_point = get_parent().castTarget.get_parent().global_position
-
-#signal
 func _on_area_entered(area):
 	if area == get_parent().castTarget:
 		isHitted = true
